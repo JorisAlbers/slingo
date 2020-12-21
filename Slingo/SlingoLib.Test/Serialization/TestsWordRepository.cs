4c4986263a6beae946e41685eb3a493192a7596f
JorisAlbers <joris_albers@hotmail.com>
2020-12-21 17:52:46 +0100

    Parse list containing words. Only words of length 5 and with basic letters are returned.

--- Slingo/SlingoLib.Test/Serialization/TestsWordRepository.cs
+++ Slingo/SlingoLib.Test/Serialization/TestsWordRepository.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using System.Text;
 using Moq;
 using NUnit.Framework;
 using SlingoLib.Serialization;
@@ -12,10 +15,28 @@ namespace SlingoLib.Test.Serialization
         [Test()]
         public void Deserialize5LetterWords_FileDoesNotExist_Throws()
         {
-            var fileMock = new Mock<IFileInfo>();
-            fileMock.Setup(x => x.Exists).Returns(false);
-            WordRepository repo = new WordRepository(fileMock.Object);
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>());
+            WordRepository repo = new WordRepository(fileSystem.FileSystem, "words.txt");
             Assert.Throws<FileNotFoundException>(() => repo.Deserialize5LetterWords());
+        }
+
+        [TestCase("thisWordIsTooLong")]
+        [TestCase("abcd5")]
+        [TestCase("Abcdf")]
+        [TestCase("ab df")]
+        [TestCase("ab-df")]
+        [TestCase("ab,df")]
+        [TestCase("abédf")]
+        [TestCase("ab|df")]
+        public void Deserialize5LetterWords_IgnoresWordsWhichAreNotAllowed(string word)
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { @"c:\words.txt", new MockFileData(word) },
+            });
+            
+            WordRepository repo = new WordRepository(fileSystem.FileSystem, @"c:\words.txt");
+            Assert.IsEmpty(repo.Deserialize5LetterWords());
         }
     }
 }
--- Slingo/SlingoLib/Serialization/WordRepository.cs
+++ Slingo/SlingoLib/Serialization/WordRepository.cs
@@ -1,26 +1,63 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.IO.Abstractions;
+using System.Linq;
 
 namespace SlingoLib.Serialization
 {
     public class WordRepository
     {
-        private readonly IFileInfo _fiveLetterWords;
+        private readonly IFileSystem _fileSystem;
+        private readonly string _filePath;
+        private const string _ALLOWED_CHARS = "abcdefghijklmnopqrstuvwxyz";
+        private readonly char[] _allowedCharsAsArray = _ALLOWED_CHARS.ToCharArray();
 
-        public WordRepository(IFileInfo fiveLetterWords)
+        public WordRepository(IFileSystem fileSystem, string filePath)
         {
-            _fiveLetterWords = fiveLetterWords;
+            _fileSystem = fileSystem;
+            _filePath = filePath;
         }
 
         public List<string> Deserialize5LetterWords()
         {
-            if (!_fiveLetterWords.Exists)
+            if (!_fileSystem.File.Exists(_filePath))
             {
                 throw new FileNotFoundException();
             }
 
-            return null;
+            List<string> words = new List<string>();
+            using (StreamReader reader = new StreamReader(_fileSystem.File.OpenRead(_filePath)))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string word = line.Substring(0, line.Length - 2); // remove \r\n
+                    if (WordIsAllowed(word))
+                    {
+                        words.Add(word);
+                    }
+                }
+            }
+
+            return words;
+        }
+
+        private bool WordIsAllowed(string line)
+        {
+            if (line.Length != 5)
+            {
+                return false;
+            }
+
+            foreach (char letter in line)
+            {
+                if (!_allowedCharsAsArray.Contains(letter))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
