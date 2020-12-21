61153aacb390c89b53e0fdf106f488d2dc3c1265
JorisAlbers <joris_albers@hotmail.com>
2020-12-21 17:11:38 +0100

    Added WordRepository.cs

---
+++ Slingo/SlingoLib.Test/Serialization/TestsWordRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.IO.Abstractions;
+using Moq;
+using NUnit.Framework;
+using SlingoLib.Serialization;
+
+namespace SlingoLib.Test.Serialization
+{
+    [TestFixture()]
+    public class TestsWordRepository
+    {
+        [Test()]
+        public void Deserialize5LetterWords_FileDoesNotExist_Throws()
+        {
+            var fileMock = new Mock<IFileInfo>();
+            fileMock.Setup(x => x.Exists).Returns(false);
+            WordRepository repo = new WordRepository(fileMock.Object);
+            Assert.Throws<FileNotFoundException>(() => repo.Deserialize5LetterWords());
+        }
+    }
+}
---
+++ Slingo/SlingoLib/Serialization/WordRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+
+namespace SlingoLib.Serialization
+{
+    public class WordRepository
+    {
+        private readonly IFileInfo _fiveLetterWords;
+
+        public WordRepository(IFileInfo fiveLetterWords)
+        {
+            _fiveLetterWords = fiveLetterWords;
+        }
+
+        public List<string> Deserialize5LetterWords()
+        {
+            if (!_fiveLetterWords.Exists)
+            {
+                throw new FileNotFoundException();
+            }
+
+            return null;
+        }
+    }
+}
