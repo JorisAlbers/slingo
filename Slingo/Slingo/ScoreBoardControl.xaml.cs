f9802c18e1b7a89989325ffc1da3fc3df9dbbb0c
JorisAlbers <joris_albers@hotmail.com>
2020-12-30 17:45:43 +0100

    Added ScoreBoard vm & view

---
+++ Slingo/Slingo/ScoreBoardControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ReactiveUI;
+
+namespace Slingo
+{
+    /// <summary>
+    /// Interaction logic for ScoreBoardControl.xaml
+    /// </summary>
+    public partial class ScoreBoardControl : ReactiveUserControl<ScoreboardViewModel>
+    {
+        public ScoreBoardControl()
+        {
+            InitializeComponent();
+
+            this.WhenActivated((dispose) =>
+            {
+                this.OneWayBind(ViewModel,
+                        vm => vm.Score,
+                        view => view.ScoreTextBlock.Text)
+                    .DisposeWith(dispose);
+
+                this.OneWayBind(ViewModel,
+                        vm => vm.IsActiveTeam,
+                        view => view.OuterBorder.BorderBrush,
+                        (isActiveTeam) => isActiveTeam ? new SolidColorBrush(Color.FromRgb(251, 189, 0)) : new SolidColorBrush(Color.FromRgb(199, 201, 203)))
+                    .DisposeWith(dispose);
+            });
+        }
+    }
+}
---
+++ Slingo/Slingo/ScoreboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace Slingo
+{
+    public class ScoreboardViewModel : ReactiveObject
+    {
+        public string TeamName { get; }
+        
+        [Reactive] public int Score { get; private set; }
+        [Reactive] public bool IsActiveTeam { get; set; }
+
+        public ScoreboardViewModel(string teamName)
+        {
+            TeamName = teamName;
+        }
+    }
+}
