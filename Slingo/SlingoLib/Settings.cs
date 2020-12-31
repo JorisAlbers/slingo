a39e15f6b481a471e0b0217977d48f1de15a5d3f
JorisAlbers <joris_albers@hotmail.com>
2020-12-31 13:10:03 +0100

    Added the property StartingTeam

--- Slingo/SlingoLib/Settings.cs
+++ Slingo/SlingoLib/Settings.cs
@@ -9,6 +9,8 @@ namespace SlingoLib
     {
         public Team Team1 { get; }
         public Team Team2 { get; }
+        public Team StartingTeam { get; }
+        
         public int WordSize { get; }
         
         /// <summary> Seconds available to come up with the next word entry. </summary>
@@ -18,11 +20,18 @@ namespace SlingoLib
         /// The number of times a SLINGO needs to be achieved before the game ends.
         /// </summary>
         public int Rounds { get; }
+        
 
-        public Settings(Team team1, Team team2, int wordSize, int timeout, int rounds)
+        public Settings(Team team1, Team team2, Team startingTeam, int wordSize, int timeout, int rounds)
         {
+            if (startingTeam != team1 && startingTeam != team2)
+            {
+                throw new ArgumentException("The starting team must be either team 1 or team 2");
+            }
+
             Team1 = team1;
             Team2 = team2;
+            StartingTeam = startingTeam;
             WordSize = wordSize;
             Timeout = timeout;
             Rounds = rounds;
