37d454e8cd21d422233568ee23851a219de96026
JorisAlbers <joris_albers@hotmail.com>
2021-01-27 21:20:29 +0100

    Fixed timing of sound fill ball long

--- Slingo/Slingo/Game/Bingo/BingoViewModel.cs
+++ Slingo/Slingo/Game/Bingo/BingoViewModel.cs
@@ -219,7 +219,8 @@ namespace Slingo.Game.Bingo
                 if(viewmodel.Number == number)
                 {
                     _audioEngine.PlaySound(_fillBallLongSound);
-                    await viewmodel.Fill();
+                    viewmodel.Fill();
+                    await Task.Delay(1084);
                     break;
                 }
             }
